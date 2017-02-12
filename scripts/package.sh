3675328a19eb14ca5dbcfe51417b16ef9c84af54
yasserf <yasser.fadl@gmail.com>
2017-02-12 15:37:20 +0100

    build: node package version now 6.9.5

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PACKAGED_NODE_VERSION="v6.9.1"
+PACKAGED_NODE_VERSION="v6.9.5"
 OS=$( node -e "console.log(require('os').platform())" )
 NODE_VERSION=$( node --version )
 COMMIT=$( git log --pretty=format:%h -n 1 )
